296386f634aaf89d083fda5661b842bb67c6891b
abhik soni <asoni88899@gmail.com>
2026-03-20 00:46:53 +0530

    Datatype

---
+++ day1_variables_and_data_type/Datatype.java
@@ -0,0 +1,12 @@
+public class Datatype {
+    public static void main(String args[]){
+        char ch = 'a';
+        System.out.println(ch);
+        boolean bol = true;
+        System.out.println(bol);
+        float flo = 6.5f;
+        System.out.println(flo);
+        double dou = 6.454;
+        System.out.print(dou);
+    }   
+}
