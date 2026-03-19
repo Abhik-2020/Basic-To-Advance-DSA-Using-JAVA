502a74ee9dcdcbae5331a36eee40a98a7d284ab6
abhik soni <asoni88899@gmail.com>
2026-03-20 00:41:08 +0530

    comments

---
+++ day1_variables_and_data_type/comments.java
@@ -0,0 +1,15 @@
+public class comments {
+        
+    public static void main(String arg[]){
+       
+      System.out.println("Hello world");
+      System.out.println("Hello world");
+      System.out.println("Hello world");
+    //               or
+      System.out.print("Hello world\n");
+      System.out.print("Hello world\n");
+      System.out.print("Hello world\n");
+   }
+    
+    
+}
