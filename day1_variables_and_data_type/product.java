31fd37a28006408ceb63850034ae0c6e95d35671
abhik soni <asoni88899@gmail.com>
2026-03-21 11:29:12 +0530

    product

---
+++ day1_variables_and_data_type/product.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class product {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ener your fisrt number");
+        int a = sc.nextInt();
+        System.out.println("Enter your second number");
+        int b = sc.nextInt();
+        int c = a*b;
+        System.out.println("product of these two number is :- "+ c );
+        
+    }
+    
+}
