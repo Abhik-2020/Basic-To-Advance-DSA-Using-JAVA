837072fbc3d0ef76a7956a9ed28a484c2bc0c1af
abhik soni <asoni88899@gmail.com>
2026-03-21 11:28:43 +0530

    input

---
+++ day1_variables_and_data_type/input_addition.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class input_addition {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your first number");
+        int a = sc.nextInt();
+        System.out.print("Enter your second number");
+        int b = sc.nextInt();
+        int c = a + b;
+        System.out.print(c);
+    }
+    
+}
