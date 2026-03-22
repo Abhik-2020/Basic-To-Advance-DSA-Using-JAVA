3c450e8a501bdfeb8568add37e13df594782327c
abhik soni <asoni88899@gmail.com>
2026-03-22 15:45:04 +0530

    operators

---
+++ day2_operators/operators.java
@@ -0,0 +1,70 @@
+public class operators{
+    public static void main(String args[]){
+        // int a = 5;
+        // int b = 10;
+         
+        // binary operator
+        // all types of binary oprators
+        // int c = a+b;
+        // int c = a-b;
+        // int c = a*b;
+        // int c = a/b;
+        // int c = a%b;
+
+        // unary operator
+        // all types of unary operators
+        // ++
+        // --
+
+        //pre increment
+        // int a = 5;
+        // int b = ++a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        //post increment
+        // int a = 5;
+        // int b = a++;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // Relational operator
+        // ==
+        // !=
+        // >
+        // <
+        // >=
+        // <=
+
+        // examples
+        // int a = 0;
+        // int b = 5;
+        // System.out.println(a==b);
+        // System.out.println(a!=b);
+        // System.out.println(a>=b);
+        // System.out.println(a<=b);
+
+
+        // logical operators
+        // && :- any one condition is flase than this is flase other wise it is true.
+
+        // || :- any one condition is true than this is true other wise it is false. 
+
+        // != :- if ture it shows false if false its out put will be true.
+
+        // examples
+        int a = 0;
+        int b = 5;
+        System.out.println(a<b && a==b);
+        System.out.println(a<b && b>a);
+
+
+
+
+
+
+        
+        
+
+    }
+}
