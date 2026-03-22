702de7af8b0c423e4707ebdc4027e656e8aa8576
abhik soni <asoni88899@gmail.com>
2026-03-22 15:48:26 +0530

    if_else

---
+++ day3_conditional_statements/if_else.java
@@ -0,0 +1,17 @@
+public class if_else{
+    public static void main(String args[]){
+        int age = 16;
+        
+        if(age >= 18){
+            System.out.print("adult, you can vote");
+        }
+        if(age >= 13 && age < 18){
+            System.out.print("you are teenager");
+        }
+        else{
+            System.out.print("you are tilte baby");
+        }
+        }
+        
+         
+     }
