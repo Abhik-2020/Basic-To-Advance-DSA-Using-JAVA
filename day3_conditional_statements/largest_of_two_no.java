98405f335cf9a2cc2b72a5cc212e1652002fe2bf
abhik soni <asoni88899@gmail.com>
2026-03-22 15:49:40 +0530

    largest_of_two_number

---
+++ day3_conditional_statements/largest_of_two_no.java
@@ -0,0 +1,15 @@
+public class largest_of_two_no {
+    public static void main(String agrs[]){
+        int a = 5;
+        int b = 3;
+        if(a>=b){
+            System.out.print("a is greater");
+
+        }
+        else{
+            System.out.print("b is grater");
+        }
+    }
+    
+    
+}
