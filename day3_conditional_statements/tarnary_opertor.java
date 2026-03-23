1701ba22b1252d3cc9a0ce62bd1c82be1a0e4623
abhik soni <asoni88899@gmail.com>
2026-03-23 11:35:06 +0530

    tarnary_opertor

---
+++ day3_conditional_statements/tarnary_opertor.java
@@ -0,0 +1,13 @@
+public class tarnary_opertor {
+    public static void main(String args[]){
+        int a = 4;
+        
+        String check = ((a%2) == 0) ? "EVEN" : "ODD";
+
+        System.out.print(check);
+
+
+
+    }
+    
+}
