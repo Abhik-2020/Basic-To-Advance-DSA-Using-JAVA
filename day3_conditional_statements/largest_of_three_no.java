4f73943418e25f486179992d27179aef8c67944e
abhik soni <asoni88899@gmail.com>
2026-03-23 11:33:56 +0530

    largest_of_three_number

---
+++ day3_conditional_statements/largest_of_three_no.java
@@ -0,0 +1,21 @@
+public class largest_of_three_no{
+    public static void main(String args[]){
+        int a = 4;
+        int b = 6;
+        int c = 8;
+        if(a >= b && a >=c){
+            System.out.print("a is larger");
+
+        }
+        else if(b >= c){
+            System.out.println("b is larger");
+
+        }
+        else{
+            System.out.println("c is larger");
+            
+        }
+    }
+
+
+}
