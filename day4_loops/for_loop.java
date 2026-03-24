8a448ff7244f43688adcadef73bf9ffba68d9926
abhik soni <asoni88899@gmail.com>
2026-03-24 11:17:37 +0530

    for_loop

---
+++ day4_loops/for_loop.java
@@ -0,0 +1,9 @@
+public class for_loop {
+    public static void main(String args[]){
+        // for(initialization; condition; upadation){work}
+        for(int i = 1; i<=10; i++){
+            System.out.println("hello world");
+        }
+    }
+    
+}
