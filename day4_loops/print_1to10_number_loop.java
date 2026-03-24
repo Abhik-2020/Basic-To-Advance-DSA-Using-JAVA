8e59c5520bd2db097aab7cc071e3ad4c7e139fac
abhik soni <asoni88899@gmail.com>
2026-03-24 11:15:57 +0530

    print_1to10_number_loop.

---
+++ day4_loops/print_1to10_number_loop.java
@@ -0,0 +1,10 @@
+public class print_1to10_number_loop{
+    public static void main(String args[]){
+        int counter = 0;
+
+        while(counter <= 10){
+            System.out.print(counter + " ");
+            counter++;
+        }
+    }
+}
