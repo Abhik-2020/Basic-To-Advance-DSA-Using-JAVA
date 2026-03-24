f839552a9718d27f0fec91390e2e8dd61f391e93
abhik soni <asoni88899@gmail.com>
2026-03-24 11:16:54 +0530

    sum_of_n_number

---
+++ day4_loops/sum_of_n_number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class sum_of_n_number {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int range = sc.nextInt();
+
+        int i = 1;
+        int sum = 0;
+
+        while(i <= range){
+            sum = sum+i;
+            i++;
+        }
+        System.out.println(sum);
+
+    }
+    
+}
