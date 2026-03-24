c0b7d53e5de1034fa61f5f33e094288b5b58ac3b
abhik soni <asoni88899@gmail.com>
2026-03-24 11:15:21 +0530

    infinite_loop

---
+++ day4_loops/infinite_loop.java
@@ -0,0 +1,11 @@
+public class infinite_loop {
+    public static void main(String args[]){
+     int counter = 0;
+     while(counter < 100){
+         System.out.println("hello world");
+         counter++;
+         //if you not add counter ++ then it will always print hello world so it is knows as infinite loop  
+     }
+    
+    }
+}
