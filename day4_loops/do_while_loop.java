dce4349970e9e51fc46bee53d615c7780eccd017
abhik soni <asoni88899@gmail.com>
2026-03-25 10:38:06 +0530

    do_while_loop

---
+++ day4_loops/do_while_loop.java
@@ -0,0 +1,16 @@
+public class do_while_loop {
+    public static void main(String args[]){
+        int counter = 0;
+        
+        do{
+            
+            System.out.println("hello world");
+            counter+=1;
+
+
+        }while(counter<=10);
+        System.out.println("10times ");
+
+    }
+    
+}
