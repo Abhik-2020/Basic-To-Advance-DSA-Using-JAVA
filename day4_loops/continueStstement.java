85b9872f518884bd7b1b7e4b1c4799b8bc619e5d
abhik soni <asoni88899@gmail.com>
2026-03-25 10:40:00 +0530

    continueStstement

---
+++ day4_loops/continueStstement.java
@@ -0,0 +1,14 @@
+// continue statement :- to skip the iteration .
+public class continueStstement {
+    public static void main(String args[]){
+        for(int i = 0; i<=10; i++){
+            if(i == 5){
+                continue;
+            }
+            System.out.println(i);
+            
+        }
+
+    }
+    
+}
