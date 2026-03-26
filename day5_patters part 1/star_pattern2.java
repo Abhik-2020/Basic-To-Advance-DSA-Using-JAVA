6a0064407e3011d043baa79fa5ffe9e961ce10e0
abhik soni <asoni88899@gmail.com>
2026-03-26 15:32:49 +0530

    star_pattern2

---
+++ day5_patters part 1/star_pattern2.java
@@ -0,0 +1,19 @@
+// Q.2
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+public class star_pattern2 {
+    public static void main(String agrs[]){
+        int n = 5;
+        for(int line=1; line<=n; line++){
+            for(int star=1; star<=(n-line+1); star++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
