0ce4156cabed25f3a56a853354ffd632c9a69155
abhik soni <asoni88899@gmail.com>
2026-03-27 10:05:40 +0530

    Question5

---
+++ day5_patters part 1/Question5.java
@@ -0,0 +1,17 @@
+//      * * * * *
+//     * * * * *
+//    * * * * *
+//   * * * * *
+//  * * * * *
+public class Question5 {
+    public static void main(String args[]){
+        for(int i=1; i<=5; i++){
+            for(int j=4; j>=i; j--){
+                System.out.print(" ");
+            }
+            for(int s=1; s<=n)
+            System.out.println("*****");
+        }
+    }
+    
+}
