66ff8e1317c282aec9c60fc7c7c6994f0298f0b6
abhik soni <asoni88899@gmail.com>
2026-03-29 15:35:56 +0530

    InRangePrimesNumber

---
+++ day6_function_methods/InRangePrimesNumber.java
@@ -0,0 +1,25 @@
+public class InRangePrimesNumber {
+    public static boolean Isprime(int n){
+        if(n==2){
+            return true;
+        }
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+    public static void PrimeInRange(int n){
+        for(int i=2; i<=n; i++){
+            if(Isprime(i)){
+                System.out.println(i);
+            }
+        }
+    }
+    public static void main(String args[]){
+        PrimeInRange(20);
+    }
+    
+}
