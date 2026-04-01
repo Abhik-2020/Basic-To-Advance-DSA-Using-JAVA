6ddfd9eb238c4421f3aeba4b9b8e1a6d817eee33
abhik soni <asoni88899@gmail.com>
2026-04-01 12:43:36 +0530

    zero_one_triangle

---
+++ day7_advance_pattern/zero_one_triangle.java
@@ -0,0 +1,28 @@
+// 1
+// 01
+// 101
+// 0101
+// 10101
+public class zero_one_triangle{
+    public static void ZeroOneTriangle(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                if((i+j)%2 == 0){
+                System.out.print("1");
+            }
+            else{
+                System.out.print("0");
+            }
+
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        ZeroOneTriangle(5);
+    }
+    
+
+
+}
